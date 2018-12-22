3840ddc595304b47de1fd0932581ebd93cc56ff0
Kim Sangyeon <dho113@naver.com>
2018-12-22 15:47:52 +0900

    add remove Tag script - contentscript.js : add remove tag script case

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,1 +1,80 @@
-console.log("content script");
+window.onload = () => {
+	console.log("===AD CLEANMER===");
+
+	/* hankookilbo ad container */
+	const elAdContainer = document.getElementsByClassName('ad-container');
+	Array.from(elAdContainer).forEach(el => el.remove());
+
+	/* asiatoday gad_wrap */
+	const elAdWap = document.getElementsByClassName('gad_wrap');
+	Array.from(elAdWap).forEach(el => el.remove());
+
+	/* asiatoday gad_wrap */
+	const elAdBox = document.getElementsByClassName('ad_box');
+	Array.from(elAdBox).forEach(el => el.remove());
+
+	/* asiatoday gad_wrap */
+	const elAdIwmads = document.getElementsByClassName('iwmads-wrapper');
+	Array.from(elAdIwmads).forEach(el => el.remove());
+
+	/* asiatoday ixx-popup-wrap */
+	const elAdIxxpopup = document.getElementsByClassName('ixx-popup-wrap');
+	Array.from(elAdIxxpopup).forEach(el => el.remove());
+
+	/* asiatoday wp_adbn_root */
+	const elAdbnRoot = document.getElementById('wp_adbn_root');
+	elAdbnRoot.remove();
+
+	/* asiatoday sliderAdText */
+	const elAdText = document.getElementById('sliderAdText');
+	elAdText.closest('#scrollDiv').remove();
+
+	/* asiatoday ad_optima_wrap */
+	const elAdOptima = document.getElementById('ad_optima_wrap');
+	elAdOptima.remove();
+
+	/* asiatoday tmpILSA_Container */
+	const elAdtmpILSA = document.getElementById('tmpILSA_Container');
+	elAdtmpILSA.remove();
+
+	const elIframe = document.getElementsByTagName('iframe');
+	Array.from(elIframe).forEach(el => {
+		/* kwangju pub.adpnut.com */
+		if (el.src.indexOf('pub.adpnut.com') > -1) {
+			el.remove();
+		}
+
+		/* asiatoday compass.adop.cc */
+		if (el.src.indexOf('compass.adop.cc') > -1) {
+			el.remove();
+		}
+
+		/* asiatoday ad.idnad.co.kr */
+		if (el.src.indexOf('ad.idnad.co.kr') > -1) {
+			el.remove();
+		}
+
+		/* asiatoday adex.ednplus.com */
+		if (el.src.indexOf('adex.ednplus.com') > -1) {
+			el.remove();
+		}
+
+		/* asiatoday adgrp */
+		if (el.src.indexOf('adgrp') > -1) {
+			el.remove();
+		}
+
+		/* asiatoday dsp.adzon.net */
+		if (el.src.indexOf('dsp.adzon.net') > -1) {
+			el.remove();
+		}
+	});
+
+	const elIns = document.getElementsByTagName('ins');
+	Array.from(elIns).forEach(el => {
+		/* asiatoday adsbyadop */
+		if (el.className.indexOf('adsbyadop') > -1) {
+			el.remove();
+		}
+	});
+};
