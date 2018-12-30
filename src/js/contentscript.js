1b0982a0f0396cd1e42dc8b15d623c46396b1175
Kim Sangyeon <dho113@naver.com>
2018-12-30 20:50:36 +0900

    task/add-consts-js
    - contentscript.js: consts {tagId, tagClass, iframeSrc}

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,3 +1,5 @@
+const {tagId, tagClass, iframeSrc} = consts;
+
 window.onload = () => {
 	console.log("===AD CLEANMER===");
 
@@ -5,38 +7,6 @@ window.onload = () => {
 	findTagToClass();
 	removeTagToName();
 };
-				
-const tagId = [
-	/* asiatoday */
-	'wp_adbn_root', 'scrollDiv', 'ad_optima_wrap', 'tmpILSA_Container', 'wingBnLeft',
-	'wingBnRight', 'scrolling_banner', 'scrolling_left_banner', 'inbnetDSP_B', 'section_aside',
-	/* dt.co.kr */
-	'topad', 'top_ad', 'noticeAd_pc_wrap', 'bnr', 'right_skyWingBanner',
-	/* newsway */
-	'aswift_0_expand', 'aswift_1_expand',
-	/* viva100 */
-	'dablewidget_GokvjEXv', 'viewElpVideo', 'I_viva100_bottomcenter_580_400',
-	/* nocutnews */
-	'pnlRightArea',
-	/* zdnet */
-	'ifgc', 'main_right_Floating_banner', 'mtMenu'
-];
-const tagClass = [
-	/* hankookilbo */
-	'ad-container',
-	/* asiatoday */
-	'gad_wrap', 'ad_box', 'iwmads-wrapper', 'ixx-popup-wrap', 'link_site', 'mt15', 'mb08',
-	/* dt.co.kr */
-	'trc_rbox_container', 'trc_related_container',
-	/* newsway */
-	'rightList', 'text_ad', 'bannerad_02',
-	/* viva100 */
-	'con_right', 'con_right2', 'ads_bottom',
-	/* nocutnews */
-	'ad_sky', 'ad_sky2', 'ad_top1', 'head_adr2',
-	/* zdnet */
-	'view_ad', 'scroll_ad', 'bn_box'
-];
 
 /**
 	Remove Tag
@@ -82,53 +52,7 @@ removeTagToClass = (className) => {
 removeTagToName = () => {
 	const elIframe = document.getElementsByTagName('iframe');
 	Array.from(elIframe).forEach(el => {
-		/* kwangju pub.adpnut.com */
-		if (el.src.indexOf('pub.adpnut.com') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday compass.adop.cc */
-		if (el.src.indexOf('compass.adop.cc') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday ad.idnad.co.kr */
-		if (el.src.indexOf('ad.idnad.co.kr') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday adex.ednplus.com */
-		if (el.src.indexOf('adex.ednplus.com') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday adgrp */
-		if (el.src.indexOf('adgrp') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday dsp.adzon.net */
-		if (el.src.indexOf('dsp.adzon.net') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday ad.adinc.k */
-		if (el.src.indexOf('ad.adinc.kr') > -1) {
-			el.remove();
-		}
-
-		/* asiatoday was.livere.me */
-		if (el.src.indexOf('was.livere.me') > -1) {
-			el.remove();
-		}
-
-		/* dt.co.kr io1.innorame.com */
-		if (el.src.indexOf('io1.innorame.com') > -1) {
-			el.remove();
-		}
-
-		/* dt.co.kr /ads/ */
-		if (el.src.indexOf('/ads/') > -1) {
+		if (iframeSrc.indexOf(el.src) > -1) {
 			el.remove();
 		}
 	});
