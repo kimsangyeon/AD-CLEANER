5eafdbceddaaa964ae1a2bac1c58aaf5ae773164
Kim Sangyeon <dho113@naver.com>
2019-01-10 23:32:30 +0900

    task/arrange-indent
    - tab -> space 4

--- src/js/const/consts.js
+++ src/js/const/consts.js
@@ -1,45 +1,45 @@
 const consts = {
-	TAG_ID: [
-		/* asiatoday */
-		'wp_adbn_root', 'scrollDiv', 'ad_optima_wrap', 'tmpILSA_Container', 'wingBnLeft',
-		'wingBnRight', 'scrolling_banner', 'scrolling_left_banner', 'inbnetDSP_B', 'section_aside',
-		/* dt.co.kr */
-		'topad', 'top_ad', 'noticeAd_pc_wrap', 'bnr', 'right_skyWingBanner',
-		/* newsway */
-		'aswift_0_expand', 'aswift_1_expand',
-		/* viva100 */
-		'dablewidget_GokvjEXv', 'viewElpVideo', 'I_viva100_bottomcenter_580_400',
-		/* nocutnews */
-		'pnlRightArea',
-		/* zdnet */
-		'ifgc', 'main_right_Floating_banner', 'mtMenu',
-		/* sedaily */
-		'v-right', 'livereAd'
-	],
-	TAG_CLASS: [
-		/* hankookilbo */
-		'ad-container',
-		/* asiatoday */
-		'gad_wrap', 'ad_box', 'iwmads-wrapper', 'ixx-popup-wrap', 'link_site', 'mt15', 'mb08',
-		/* dt.co.kr */
-		'trc_rbox_container', 'trc_related_container',
-		/* newsway */
-		'rightList', 'text_ad', 'bannerad_02',
-		/* viva100 */
-		'con_right', 'con_right2', 'ads_bottom',
-		/* nocutnews */
-		'ad_sky', 'ad_sky2', 'ad_top1', 'head_adr2',
-		/* zdnet */
-		'view_ad', 'scroll_ad', 'bn_box', 'top_bn_zone', 'bitcoin_box', 'power_box',
-		/* sedaily */
-		'main_wing_banner', 'sub_ad_banner3', 'sub_ad_banner3_2', 'sub_ad_banner4', 'sub_ad_banner7', 'sub_ad_banner7_2'
-	],
-	IFRAME_SRC: [
-		/* kwangju */
-		'pub.adpnut.com',
-		/* asiatoday */
-		'compass.adop.cc', 'ad.idnad.co.kr', 'adex.ednplus.com', 'adgrp', 'dsp.adzon.net', 'ad.adinc.kr', 'was.livere.me',
-		/* dt.co.kr */
-		'io1.innorame.com', '/ads/'
-	]
+    TAG_ID: [
+        /* asiatoday */
+        'wp_adbn_root', 'scrollDiv', 'ad_optima_wrap', 'tmpILSA_Container', 'wingBnLeft',
+        'wingBnRight', 'scrolling_banner', 'scrolling_left_banner', 'inbnetDSP_B', 'section_aside',
+        /* dt.co.kr */
+        'topad', 'top_ad', 'noticeAd_pc_wrap', 'bnr', 'right_skyWingBanner',
+        /* newsway */
+        'aswift_0_expand', 'aswift_1_expand',
+        /* viva100 */
+        'dablewidget_GokvjEXv', 'viewElpVideo', 'I_viva100_bottomcenter_580_400',
+        /* nocutnews */
+        'pnlRightArea',
+        /* zdnet */
+        'ifgc', 'main_right_Floating_banner', 'mtMenu',
+        /* sedaily */
+        'v-right', 'livereAd'
+    ],
+    TAG_CLASS: [
+        /* hankookilbo */
+        'ad-container',
+        /* asiatoday */
+        'gad_wrap', 'ad_box', 'iwmads-wrapper', 'ixx-popup-wrap', 'link_site', 'mt15', 'mb08',
+        /* dt.co.kr */
+        'trc_rbox_container', 'trc_related_container',
+        /* newsway */
+        'rightList', 'text_ad', 'bannerad_02',
+        /* viva100 */
+        'con_right', 'con_right2', 'ads_bottom',
+        /* nocutnews */
+        'ad_sky', 'ad_sky2', 'ad_top1', 'head_adr2',
+        /* zdnet */
+        'view_ad', 'scroll_ad', 'bn_box', 'top_bn_zone', 'bitcoin_box', 'power_box',
+        /* sedaily */
+        'main_wing_banner', 'sub_ad_banner3', 'sub_ad_banner3_2', 'sub_ad_banner4', 'sub_ad_banner7', 'sub_ad_banner7_2'
+    ],
+    IFRAME_SRC: [
+        /* kwangju */
+        'pub.adpnut.com',
+        /* asiatoday */
+        'compass.adop.cc', 'ad.idnad.co.kr', 'adex.ednplus.com', 'adgrp', 'dsp.adzon.net', 'ad.adinc.kr', 'was.livere.me',
+        /* dt.co.kr */
+        'io1.innorame.com', '/ads/'
+    ]
 };
